0148a83a6fcb87ecfb08845485ab3cd1ed5c8792
Jorge Gabriel Rodrigues Ferreira <jorgegabrielrf@gmail.com>
2025-03-27 00:28:18 -0300

    Só ajustei alguns erros

--- imunemais-login/src/app/app.component.ts
+++ imunemais-login/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultLoginScreenComponent } from "./components/default-login-screen/default-login-screen.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DefaultLoginScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
