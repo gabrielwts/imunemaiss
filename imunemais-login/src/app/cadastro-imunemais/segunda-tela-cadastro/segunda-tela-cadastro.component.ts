d4149105efa53ef10d5e206ab5be5648d704f403
Jorge Gabriel Rodrigues Ferreira <jorgegabrielrf@gmail.com>
2025-04-01 02:05:38 -0300

    Telas de cadastro criadas

---
+++ imunemais-login/src/app/cadastro-imunemais/segunda-tela-cadastro/segunda-tela-cadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segunda-tela-cadastro',
+  imports: [],
+  templateUrl: './segunda-tela-cadastro.component.html',
+  styleUrl: './segunda-tela-cadastro.component.scss'
+})
+export class SegundaTelaCadastroComponent {
+
+}
